49f977a6134b665c41a2f72083d4c58dac1a4e33
Moises <moises.spindola@gmail.com>
2021-02-05 17:13:22 -0800

    sets all files and gems in run and envionment

---
+++ config/environment.rb
@@ -0,0 +1,7 @@
+require 'pry'
+require 'httparty'
+require 'terminal-table'
+
+require_relative('../lib/api')
+require_relative('../lib/controller')
+require_relative('../lib/stories')
--- lib/api.rb
+++ lib/api.rb
@@ -1,4 +1,9 @@
 class API
 
+    # sesame = wGPYpFwhGgzZ0zrugkltaT88AH7pMKKg
+    # #get API data to a hash
+    # url = https://api.nytimes.com/svc/topstories/v2/{section}.json?api-key={sesame}
+
+
 
 end
