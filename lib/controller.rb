39aa73f8053a8c42c863d5205f178e9757b1eff5
Moises <moises.spindola@gmail.com>
2021-02-05 17:45:44 -0800

    adds controller instance method

--- lib/controller.rb
+++ lib/controller.rb
@@ -1,4 +1,15 @@
 class Controller
 
+    def start
+    puts ""
+    puts "           Welcome to           "
+    puts ""
+    puts "THE NEW YORK TIMES - TOP STORIES"
+    puts ""
+    puts "Which section would you like to view?"
+
+    
+
+    end
 
 end
