d78b5370d5e7f293102ddae05c3d27633013fddc
Moises <moises.spindola@gmail.com>
2021-02-11 10:49:16 -0800

    adds comments

--- lib/api.rb
+++ lib/api.rb
@@ -1,11 +1,11 @@
-class Api
-    @@sesame = "wGPYpFwhGgzZ0zrugkltaT88AH7pMKKg"
+class Api 
+    @@sesame = "wGPYpFwhGgzZ0zrugkltaT88AH7pMKKg" #Provides api key 
         
-    def articles_list(input)
-        url = "https://api.nytimes.com/svc/topstories/v2/#{input}.json?api-key=#{@@sesame}"
-        response = HTTParty.get(url)
-        response["results"].map do |i|
-            Stories.new({title: i["title"], abstract: i["abstract"], url: i["url"]})
+    def articles_list(input) # defines instance method
+        url = "https://api.nytimes.com/svc/topstories/v2/#{input}.json?api-key=#{@@sesame}" #sets url value to api url and interpolates key and input to look for in sections
+        response = HTTParty.get(url) #defines response variable with HTTParty to use get method and retrieves data
+        response["results"].map do |i| #map is used in the response variable, accessing the results hash and bringing data over to instantiate our Stories class looks for:
+            Stories.new({title: i["title"], abstract: i["abstract"], url: i["url"]}) #Stories is isntantiated with title abstract and url attributes accessing them in the returned hash. 
         end
     end
 end
