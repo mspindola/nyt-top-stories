eac7983f5ff09249f7978dbb82b6e23000bb8b8d
Moises <moises.spindola@gmail.com>
2021-02-11 10:58:51 -0800

    adds comments

--- lib/stories.rb
+++ lib/stories.rb
@@ -5,26 +5,27 @@ class Stories #classes need to be singular
     @@titles = []
     @@abstract = []
   
-    def initialize(article_attributes)
-        article_attributes.each do |key, value|
+    def initialize(article_attributes) #initializes stories class
+        #binding.pry
+        article_attributes.each do |key, value| #iterates over the hash and its k vs 
             #self.class.attr_accessor(key)   This does a repeat of attr accessor
-            self.send("#{key}=", value) if self.respond_to?("#{key}=")
+            self.send("#{key}=", value) if self.respond_to?("#{key}=") # send method invokes key as it is a object/class method
         end
-        @@titles << @title
-        @@abstract << @abstract
-        @@url << @url
+        @@titles << @title #shovels
+        @@abstract << @abstract #shovels
+        @@url << @url #shovels
         #binding.pry
     end
   
-    def self.top_5_titles
-        @@titles[0..4]
+    def self.top_5_titles #defines class method
+        @@titles[0..4] #calls out range in array of titles
     end
 
-    def self.link
-        @@url[0..4]
+    def self.link #defines class method 
+        @@url[0..4] #calls out range in array of links
     end
 
-    def self.clear
+    def self.clear #clears array for new section
         @@url.clear 
         @@titles.clear
         @@abstract.clear
